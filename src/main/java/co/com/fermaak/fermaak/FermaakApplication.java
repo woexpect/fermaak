d04d8ada369c174983489f10482a6dfb1e17a9d5
woexpect <sebasp95@hotmail.com>
2017-12-08 17:26:25 -0500

    ajuste a java

---
+++ src/main/java/co/com/fermaak/fermaak/FermaakApplication.java
@@ -0,0 +1,12 @@
+package co.com.fermaak.fermaak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FermaakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FermaakApplication.class, args);
+	}
+}
